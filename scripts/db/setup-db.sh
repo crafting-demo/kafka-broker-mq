9da4fadff38721605d78f81fe1ecf6cacafe3986
Alexander Maru <i@alexandermaru.com>
2022-05-22 01:09:13 +0000

    use dependency snapshots

--- scripts/db/setup-db.sh
+++ scripts/db/setup-db.sh
@@ -3,4 +3,13 @@
 ROOT="$(dirname "$0")"
 cd "$ROOT"
 
+# Update system
+sudo apt-get update
+
+# Install libgmp3
+sudo apt-get install libgmp3-dev -y
+
+# Install mysql client
+sudo apt-get install mysql-client libmysqlclient-dev -y
+
 ./mysql.sh
