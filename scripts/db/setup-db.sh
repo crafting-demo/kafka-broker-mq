698660804b74bf5bd466632460bc8119c3b2f46e
Alexander Maru <i@alexandermaru.com>
2022-06-10 04:48:50 +0000

    cs wait service mysql

--- scripts/db/setup-db.sh
+++ scripts/db/setup-db.sh
@@ -12,4 +12,7 @@ sudo apt-get install libgmp3-dev -y
 # Install mysql client
 sudo apt-get install mysql-client libmysqlclient-dev -y
 
+# Wait for mysql service
+cs wait service mysql
+
 ./mysql.sh
