70612cde128e156607b0b8395adf70db77785314
Alexander Maru <i@alexandermaru.com>
2022-06-10 05:46:55 +0000

    update

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -6,11 +6,11 @@ cd "$ROOT"
 # Update system
 sudo apt-get update
 
-# Setup/prepare all databases.
-./db/setup-db.sh
-
 # Download/extract kafka.
 ./kafka/get-kafka.sh
 
 # Build go server.
 ./go-server.sh
+
+# Setup/prepare all databases.
+./db/setup-db.sh
